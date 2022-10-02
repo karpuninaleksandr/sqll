3c85c8642133e320e898c7baa97f690b7b001c58
Александр <skarper_sk@MacBook-Pro-Aleksandr.local>
2022-10-02 20:45:08 +0300

    Done task

--- src/main/java/ru/ac/uniyar/databasescourse/DatabaseExample.java
+++ src/main/java/ru/ac/uniyar/databasescourse/DatabaseExample.java
@@ -1,14 +1,10 @@
 package ru.ac.uniyar.databasescourse;
 
-
 import ru.ac.uniyar.databasescourse.utils.CsvDataLoader;
-import ru.ac.uniyar.databasescourse.utils.CsvParserString;
 
 import java.io.IOException;
 import java.nio.file.Path;
 import java.sql.*;
-import java.util.ArrayList;
-import java.util.List;
 
 public class DatabaseExample {
     private static final String URL = String.format("jdbc:mariadb://%s", System.getenv("MARIADB_HOST"));
@@ -37,18 +33,19 @@ public class DatabaseExample {
     }
 
     public static void selectSQL(String name, String query, String additionalQuery) {
-        ResultSet rs = createQuery((additionalQuery == null) ? ("SELECT " + query + " FROM students")
-                : ("SELECT " + query + " FROM students WHERE " + additionalQuery));
+        ResultSet rs = createQuery((additionalQuery == null) ? ("SELECT " + query + " FROM " + name)
+                : ("SELECT " + query + " FROM " + name + " WHERE " + additionalQuery));
+        int amountOfColumns = (name.equals("solutions")) ? 4 : 3;
         try {
             while (rs.next()) {
-                for (int i = 1; i < 8; ++i) {
+                for (int i = 1; i <= amountOfColumns; ++i) {
                     try {
                         System.out.printf("%s ", rs.getString(i));
                     } catch (SQLException ex) {
                         break;
                     }
                 }
-                System.out.println("");
+                System.out.println();
             }
         } catch (SQLException ex) {
             System.out.printf("Statement execution error: %s\n", ex);
@@ -56,8 +53,8 @@ public class DatabaseExample {
     }
 
     public static void insertSQL(String name, String columns, String values) {
-        ResultSet rs = createQuery("INSERT INTO " + name + " (" + columns +
-                " )\n VALUES " + values);
+        ResultSet rs = createQuery("INSERT IGNORE INTO " + name + " (" + columns +
+                ")\n VALUES (" + values + ")");
         try {
             while (rs.next()) System.out.printf("Statement %s\n", rs.getString(1));
         } catch (SQLException ex) {
@@ -66,6 +63,7 @@ public class DatabaseExample {
     }
 
     public static void createTablesSQL() {
+        System.out.println("Creating new tables...");
         createQuery("CREATE TABLE IF NOT EXISTS students(\n" +
                 "studentID INT PRIMARY KEY,\n" +
                 "studentName CHAR(127) NOT NULL,\n" +
@@ -89,35 +87,47 @@ public class DatabaseExample {
                 ")\n" +
                 "CHARACTER SET utf8\n" +
                 "COLLATE utf8_general_ci;");
-//        CsvDataLoader.parseCsvFile(Path.of("data.csv")).forEach(csvParserString -> {
-//            insertSQL("students", "studentId, studentName, studentSurname",
-//                    csvParserString.getStudentId() + " " + csvParserString.getStudentName() + " "
-//                            + csvParserString.getStudentSurname());
-//            insertSQL("solutions", "solutionID, studentId, reviewerID, score, hasPassed",
-//                    csvParserString.getSolutionId() + " " + csvParserString.getStudentId() + " "
-//                            + csvParserString.getReviewerId() + " " + csvParserString.getScore() + " "
-//                            + csvParserString.getHasPassed());
-//            insertSQL("reviewers", "reviewerID, reviewerSurname, reviewerDepartment",
-//                    csvParserString.getReviewerId() + " " + csvParserString.getReviewerSurname() + " "
-//                            + csvParserString.getReviewerDepartment());
-//        });
+        System.out.println("Created new tables");
     }
 
     public static void dropTablesSQL() {
+        System.out.println("Dropping all tables...");
         createQuery("DROP TABLE students");
         createQuery("DROP TABLE solutions");
         createQuery("DROP TABLE reviewers");
+        System.out.println("Dropped all tables");
+    }
+
+    public static void fillTablesSQL() {
+        System.out.println("Filling tables from csv file...");
+        CsvDataLoader.parseCsvFile(Path.of("data.csv")).forEach(csvParserString -> {
+            insertSQL("students", "studentId, studentName, studentSurname",
+                    csvParserString.getStudentId() + ", \"" + csvParserString.getStudentName() + "\", \""
+                            + csvParserString.getStudentSurname() + "\"");
+            insertSQL("solutions", "solutionID, studentId, reviewerID, score, hasPassed",
+                    csvParserString.getSolutionId() + ", " + csvParserString.getStudentId() + ", "
+                            + csvParserString.getReviewerId() + ", " + csvParserString.getScore() + ", \""
+                            + csvParserString.getHasPassed() + "\"");
+            insertSQL("reviewers", "reviewerID, reviewerSurname, reviewerDepartment",
+                    csvParserString.getReviewerId() + ", \"" + csvParserString.getReviewerSurname() + "\", \""
+                            + csvParserString.getReviewerDepartment() + "\"");
+        });
+        System.out.println("Filled all tables");
     }
 
     public static void main(String[] args) throws IOException {
-//        dropTablesSQL();
+        dropTablesSQL();
         createTablesSQL();
-        ResultSet rs = createQuery("SHOW TABlES");
-        try {
-            while (rs.next()) System.out.printf("Statement %s\n", rs.getString(1));
-        } catch (SQLException ex) {
-            System.out.printf("Statement execution error: %s\n", ex);
-        }
+        fillTablesSQL();
+        selectSQL("students", "*", null);
+        selectSQL("solutions", "*", null);
+        selectSQL("reviewers", "*", null);
+//        ResultSet rs = createQuery("SHOW TABlES");
+//        try {
+//            while (rs.next()) System.out.printf("Statement %s\n", rs.getString(1));
+//        } catch (SQLException ex) {
+//            System.out.printf("Statement execution error: %s\n", ex);
+//        }
     }
 
     private static void checkHasPassIsNull() {
